8ef360a75433101a84a747cb36e36eba885e5652
orjlove <303650201@qq.com>
2013-12-16 13:00:15 +0800

    orjlove

---
+++ aboutUsWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HSMS.UI.softWHelpWindow
+{
+    /// <summary>
+    /// aboutUsWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class aboutUsWindow : Window
+    {
+        public aboutUsWindow()
+        {
+            InitializeComponent();
+
+            ImageBrush b = new ImageBrush();
+
+            b.ImageSource = new BitmapImage(new Uri("pack://application:,,,/backgroundImage/manage1.jpg"));
+
+            b.Stretch = Stretch.Fill;
+
+            this.Background = b;
+        }
+    }
+}
